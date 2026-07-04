d48bd119eb69ed8926c069885d4d40174bbcb0e8
antoniovalentini <antonio.valentini@live.it>
2026-07-04 22:20:06 +0200

    Guard DebugDeviceInfoViewModel's App.Instance reads for design-time safety

--- src/MobileApp/ViewModels/DebugDeviceInfoViewModel.cs
+++ src/MobileApp/ViewModels/DebugDeviceInfoViewModel.cs
@@ -4,9 +4,9 @@ namespace MobileApp.ViewModels;
 
 public partial class DebugDeviceInfoViewModel : ViewModelBase
 {
-    public string AppVersion { get; } = App.Instance.AppVersion;
-    public string DeviceId { get; } = App.Instance.DeviceId;
-    public string DeviceName { get; } = App.Instance.DeviceName;
-    public string DeviceType { get; } = App.Instance.DeviceType;
+    public string AppVersion { get; } = global::Avalonia.Controls.Design.IsDesignMode ? "design" : App.Instance.AppVersion;
+    public string DeviceId { get; } = global::Avalonia.Controls.Design.IsDesignMode ? "design-device-id" : App.Instance.DeviceId;
+    public string DeviceName { get; } = global::Avalonia.Controls.Design.IsDesignMode ? "Design Device" : App.Instance.DeviceName;
+    public string DeviceType { get; } = global::Avalonia.Controls.Design.IsDesignMode ? "Design OS" : App.Instance.DeviceType;
     public string RuntimeVersion { get; } = Environment.Version.ToString();
 }
